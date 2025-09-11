1c361ae634310dd972fc803dc2352880405c55e3
Thomas Martinsen <thomas.martinsen@virinco.com>
2025-09-11 14:03:27 +0200

    Add project files.

---
+++ ConverterTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Virinco.WATS.Interface;
+
+namespace Saki
+{
+    [TestClass]
+    public class ConverterTests : TDM
+    {
+        [TestMethod]
+        public void SetupClient()
+        {
+            SetupAPI(null, "location", "purpose", true);
+            InitializeAPI(true);
+        }
+
+        [TestMethod]
+        public void TestXMLConverter()
+        {
+        
+            InitializeAPI(true);
+            SakiXMLConverter converter = new SakiXMLConverter();
+            string fileName = "Data\\SAKI-testdata.xml";
+            SetConversionSource(new FileInfo(fileName), converter.ConverterParameters, null);
+            using (FileStream file = new FileStream(fileName, FileMode.Open))
+            {
+        
+                Report uut = converter.ImportReport(this, file);
+                Submit(uut);
+            }
+        }
+    }
+}
